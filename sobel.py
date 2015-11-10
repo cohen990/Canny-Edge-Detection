a96b01a8afec4037c452d76483b0081cdcefab99
Dan Cohen <dan.cohen@euromoneyplc.com>
2015-11-10 10:28:29 +0000

    sobel finished and partially functional#

--- sobel.py
+++ sobel.py
@@ -1,6 +1,8 @@
 import binascii
+import math
 
-img = open("test.bmp", "rb");
+img = open("moonroad.bmp", "rb");
+out_file = open("edges.bmp", "wb")
 
 stream = []
 output = []
@@ -71,21 +73,80 @@ try:
 
 			stream.append(row)
 
-
+		Gx_array = []
+		
 		for i in range(width):
 			row = []
 			for j in range(height):
 				calcedGx = 0
-				for Gi in range(3):
-					for Gj in range(3):
+				lower_i = 0
+				upper_i = 3
+				lower_j = 0
+				upper_j = 3
+				if(i == 0):
+					lower_i += 1
+				if(j == 0):
+					lower_j += 1
+				if(i == width - 1):
+					upper_i -= 1
+				if(j == height - 1):
+					upper_j -= 1
+				for Gi in range(lower_i, upper_i):
+					for Gj in range(lower_j, upper_j):
 						try:
 							calcedGx += Gx[Gi][Gj] * stream[i + Gi - 1][j + Gj - 1]
 						except IndexError:
 							print("Index Error")
-				row.append(calcedGx)
+				row.append(abs(calcedGx))
+			Gx_array.append(row)
+		
+		Gy_array = []
+		for i in range(width):
+			row = []
+			for j in range(height):
+				calcedGy = 0
+				lower_i = 0
+				upper_i = 3
+				lower_j = 0
+				upper_j = 3
+				if(i == 0):
+					lower_i += 1
+				if(j == 0):
+					lower_j += 1
+				if(i == width - 1):
+					upper_i -= 1
+				if(j == height - 1):
+					upper_j -= 1
+				for Gi in range(lower_i, upper_i):
+					for Gj in range(lower_j, upper_j):
+						try:
+							calcedGy += Gy[Gi][Gj] * stream[i + Gi - 1][j + Gj - 1]
+						except IndexError:
+							print("Index Error")
+				row.append(abs(calcedGy))
+			Gy_array.append(row)
+
+		max = 0;
+
+		for i in range(len(Gx_array)):
+			row = []
+			for j in range(len(Gx_array[i])):
+				g = int(math.sqrt(math.pow(Gx_array[i][j], 2) + math.pow(Gy_array[i][j], 2)))
+				if(g > max):
+					max = g
+				row.append(g)
 			output.append(row)
 
+		img.seek(0)
+		for i in range(startOfData):
+			byte = img.read(1)
+			out_file.write(byte)
+
+		for i in range(width):
+			for j in range(height):
+				normalized = int((output[i][j]/ max) * 255)
+				byte = bytes([normalized])
+				out_file.write(byte)
 finally:
 	img.close()
-
-print(output)
+	out_file.close()
